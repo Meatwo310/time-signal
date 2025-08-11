45bd96c169e6f1c6e991f0ab084860b7baf319ad
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-08-11 21:12:34 +0900

    fix: VOICEVOXのバージョンチェックのエラーメッセージを改善

--- src/voicevox.rs
+++ src/voicevox.rs
@@ -15,9 +15,10 @@ fn get_voicevox_version(url: &Url) -> anyhow::Result<String> {
 pub fn check_voicevox_version(url: &Url) -> anyhow::Result<()> {
     let required_version = VersionReq::parse(">=0.24.0")?;
     let current_version = Version::parse(&get_voicevox_version(&url)?)?;
-    if !required_version.matches(&current_version) {
-        bail!("VOICEVOX version {} does not satisfy the required version {}", current_version, required_version);
+    if required_version.matches(&current_version) {
+        println!("VOICEVOX: {}", current_version);
+    } else {
+        eprintln!("VOICEVOX {} does not satisfy the required version: {}", current_version, required_version);
     }
-    println!("VOICEVOX: {}", current_version);
     Ok(())
 }
