0e6153c5d3d1ccdb78a94134f2420810c8874776
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-09-14 14:40:31 +0900

    buildはwindowsだけで

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
 extern crate embed_resource;
 
+#[cfg(windows)]
 fn main() {
     embed_resource::compile("tray-icon.rc", embed_resource::NONE).manifest_optional().unwrap();
 }
+
+#[cfg(not(windows))]
+fn main() {}
